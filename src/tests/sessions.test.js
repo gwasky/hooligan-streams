d98f2fe26942bff2be8da243f12a754466f005db
gibson <gwasukira@gmail.com>
2022-07-30 20:25:58 +0300

    implement test cases

--- src/tests/sessions.test.js
+++ src/tests/sessions.test.js
@@ -1,25 +1,36 @@
+const utils = require("./../utils/utils.js");
+
 var activeSessions = {
   user_1: [
-    { stream_id: "epl_1", last_accessed: new Date() },
-    { stream_id: "epl_2", last_accessed: new Date() },
+    { stream_id: "EPL_1", last_accessed: 20220730080000 },
+    { stream_id: "EPL_2", last_accessed: 20220730080001 },
   ],
   user_2: [
-    { stream_id: "epl_1", last_accessed: new Date() },
-    { stream_id: "epl_2", last_accessed: new Date() },
+    { stream_id: "EPL_1", last_accessed: 20220730080000 },
+    { stream_id: "EPL_2", last_accessed: 20220730080001 },
   ],
   user_3: [
-    { stream_id: "epl_1", last_accessed: new Date() },
-    { stream_id: "epl_2", last_accessed: new Date() },
-    { stream_id: "bundesliga_1", last_accessed: new Date() },
+    { stream_id: "EPL_1", last_accessed: 20220730080000 },
+    { stream_id: "EPL_2", last_accessed: 20220730080001 },
+    { stream_id: "BUNDES_1", last_accessed: 20220730080002 },
   ],
 };
 
-beforeAll(() => {});
+// beforeAll(() => {});
 
-test("Should add session object to redis", () => {
-  console.log(activeSessions);
+test("Should cache session for user_1", async () => {
+  //   console.log(activeSessions["user_1"]);
+  const status = await utils.cacheUserStream(
+    "user_1",
+    JSON.stringify(activeSessions["user_1"])
+  );
+  expect(status).toBe(true);
 });
 
-test("Should return list of session object from redis", () => {});
+test("Should return list of session object from redis", async () => {
+  const streams = await utils.getActiveStreams("user_1");
+  deserializedStreams = JSON.parse(streams);
+  expect(deserializedStreams).toEqual(activeSessions["user_1"]);
+});
 
-test("Should update session object in redis", () => {});
+test("Should update session object in redis", async () => {});
--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,1 +1,46 @@
+const redis = require("redis");
 
+let redisClient;
+
+(async () => {
+  redisClient = redis.createClient();
+  redisClient.on("error", (error) => console.error(`Error : ${error}`));
+  await redisClient.connect();
+})();
+
+const cacheUserStream = async (user_id, data) => {
+  try {
+    await redisClient.set(user_id, data);
+    return true;
+  } catch (e) {
+    return false;
+  }
+};
+
+const getActiveStreams = async (user_id) => {
+  try {
+    const sessions = await redisClient.get(user_id);
+    return sessions;
+  } catch (e) {
+    return False;
+  }
+};
+
+// const getActiveStreams = (user_id) => {
+//   return new Promise((resolve, reject) => {
+//     redisClient
+//       .get(user_id)
+//       .then((sessions) => {
+//         // console.log(sessions);
+//         resolve(sessions);
+//       })
+//       .catch((error) => {
+//         reject(error);
+//       });
+//   });
+// };
+
+module.exports = {
+  cacheUserStream: cacheUserStream,
+  getActiveStreams: getActiveStreams,
+};
